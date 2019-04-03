e193f719993e0dd72cb4d10d61ab1d9a70e179e4
mr1e14 <mr1e14@soton.ac.uk>
2019-04-03 21:35:09 +0100

    add new components for weather widget

---
+++ src/client/components/WeatherIcon.js
@@ -0,0 +1,39 @@
+import React from "react";
+import matches from "../mediaQuery";
+import clearDay from "../img/clear-day.png";
+import clearNight from "../img/clear-night.png";
+import cloudy from "../img/cloudy.png";
+import defaultIcon from "../img/default.png";
+import fog from "../img/fog.png";
+import partlyCloudDay from "../img/partly-cloudy-day.png";
+import partlyCloudyNight from "../img/partly-cloudy-night.png";
+import rain from "../img/rain.png";
+import sleet from "../img/sleet.png";
+import snow from "../img/snow.png";
+import thunderstorm from "../img/thunderstorm.png";
+import tornado from "../img/tornado.png";
+import wind from "../img/wind.png";
+
+const icons = {
+  "clear-day": clearDay,
+  "clear-night": clearNight,
+  cloudy,
+  defaultIcon,
+  fog,
+  "partly-cloudy-day": partlyCloudDay,
+  "partly-cloudy-night": partlyCloudyNight,
+  rain,
+  sleet,
+  snow,
+  thunderstorm,
+  tornado,
+  wind
+};
+
+const WeatherIcon = props => {
+  const icon = props.weatherData ? props.weatherData.icon : "defaultIcon";
+  const iconSize = matches("xs") ? "32px" : "48px";
+  return <img src={icons[icon]} height={iconSize} />;
+};
+
+export default WeatherIcon;
