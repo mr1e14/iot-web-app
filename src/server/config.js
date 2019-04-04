d306e3aa673e525d8392cb19a2aa30bdafa81b0d
mr1e14 <mr1e14@soton.ac.uk>
2019-04-04 20:16:52 +0100

    cache weather data

--- src/server/config.js
+++ src/server/config.js
@@ -1,4 +1,7 @@
 module.exports = {
   DARK_SKY_URL: "https://api.darksky.net/forecast/",
-  DARK_SKY_API_OPTIONS: "/51.4545,2.5879?exclude=minutely,hourly,daily&units=si"
+  DARK_SKY_API_OPTIONS:
+    "/51.4545,2.5879?exclude=minutely,hourly,daily&units=si",
+  WEATHER_CACHE_REFRESH_AFTER_SECONDS: 900,
+  WEATHER_CACHE_DELETE_AFTER_SECONDS: 3600
 };
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 const { getWeatherData } = require("./connectors/dark-sky-api");
+const { getCache } = require("./services/cache.service");
+const {
+  WEATHER_CACHE_REFRESH_AFTER_SECONDS,
+  WEATHER_CACHE_DELETE_AFTER_SECONDS
+} = require("./config");
 
 const app = express();
+
+const weatherCache = getCache(
+  WEATHER_CACHE_REFRESH_AFTER_SECONDS,
+  WEATHER_CACHE_DELETE_AFTER_SECONDS,
+  getWeatherData
+);
 
 app.use(express.static("dist"));
 
@@ -10,7 +21,7 @@ app.get("/api/getWeatherData", async (req, res) => {
   // TODO logging service?
   let weatherData = null;
   try {
-    weatherData = await getWeatherData();
+    weatherData = await weatherCache.get("data");
   } catch (err) {
     console.log(err);
   }
