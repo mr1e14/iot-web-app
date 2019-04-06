58f245e0edfaefefede6feaa15e64547b79da38c
mr1e14 <mr1e14@soton.ac.uk>
2019-04-06 23:31:14 +0100

    normalise grid and theme

--- src/client/components/Page.js
+++ src/client/components/Page.js
@@ -8,23 +8,64 @@ import PropTypes from "prop-types";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import { Layout } from "./";
 
-const styles = theme => ({
-  root: {
-    flexGrow: 1
-  },
-  child: {
-    padding: theme.spacing.unit * 2,
-    textAlign: "center"
-  }
-});
-
 const theme = createMuiTheme({
   palette: {
     type: "dark",
-    primary: { main: "#11cb5f" }, // Purple and green play nicely together.
-    secondary: { main: "#11cb5f" } // This is just green.A700 as hex.
+    primary: { main: "#11cb5f" },
+    secondary: { main: "#52cafe" }
   },
-  typography: { useNextVariants: true }
+  typography: {
+    useNextVariants: true
+  }
+});
+
+const styles = theme => ({
+  "@global": {
+    span: {
+      fontSize: 18,
+      [theme.breakpoints.up("sm")]: {
+        fontSize: 22
+      }
+    }
+  },
+  root: {
+    flexGrow: 1,
+    textAlign: "center"
+  },
+  childGridLeft: {
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+    paddingLeft: theme.spacing.unit * 4,
+    paddingRight: theme.spacing.unit,
+    [theme.breakpoints.up("md")]: {
+      paddingLeft: theme.spacing.unit * 8,
+      paddingRight: theme.spacing.unit * 2
+    },
+    [theme.breakpoints.up("lg")]: {
+      paddingLeft: theme.spacing.unit * 16,
+      paddingRight: theme.spacing.unit * 4
+    }
+  },
+  childGridRight: {
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+    paddingRight: theme.spacing.unit * 4,
+    paddingLeft: theme.spacing.unit,
+    [theme.breakpoints.up("md")]: {
+      paddingLeft: theme.spacing.unit * 2,
+      paddingRight: theme.spacing.unit * 8
+    },
+    [theme.breakpoints.up("lg")]: {
+      paddingLeft: theme.spacing.unit * 4,
+      paddingRight: theme.spacing.unit * 16
+    }
+  },
+  paper1: {
+    backgroundColor: "#64dd17"
+  },
+  paper2: {
+    backgroundColor: "#d500f9"
+  }
 });
 
 const Page = props => {
