a267e5b1b7b03d6c891ae45537774bde3404b5ec
mr1e14 <mr1e14@soton.ac.uk>
2019-04-16 23:24:49 +0100

    add logging service

--- src/server/config.js
+++ src/server/config.js
@@ -3,5 +3,6 @@ module.exports = {
   DARK_SKY_API_OPTIONS:
     "/51.4545,2.5879?exclude=minutely,hourly,daily&units=si",
   WEATHER_CACHE_REFRESH_AFTER_SECONDS: 900,
-  WEATHER_CACHE_DELETE_AFTER_SECONDS: 3600
+  WEATHER_CACHE_DELETE_AFTER_SECONDS: 3600,
+  LOG_DIR: "logs"
 };
---
+++ src/server/services/logging.service.js
@@ -0,0 +1,38 @@
+const { createLogger, format, transports } = require("winston");
+const colorizer = format.colorize();
+require("winston-daily-rotate-file");
+const { LOG_DIR } = require("../config");
+
+const logger = createLogger({
+  level: "info",
+  format: format.combine(
+    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    format.simple(),
+    format.printf(msg => `${msg.timestamp} - ${msg.level}:  ${msg.message}`)
+  ),
+  transports: [
+    new transports.DailyRotateFile({
+      filename: "app.log.%DATE%",
+      dirname: LOG_DIR
+    }),
+    new transports.DailyRotateFile({
+      level: "error",
+      filename: "error.log.%DATE%",
+      dirname: LOG_DIR
+    })
+  ]
+});
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new transports.Console({
+      format: format.printf(msg =>
+        colorizer.colorize(
+          msg.level,
+          `${msg.timestamp} - ${msg.level}: ${msg.message}`
+        )
+      )
+    })
+  );
+}
+
+module.exports = logger;
