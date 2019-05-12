ee0590da37239457e78daeb60a2b5007580f192f
mr1e14 <mr1e14@soton.ac.uk>
2019-05-12 23:25:28 +0100

    tidy MaterialIcon

--- src/client/components/MaterialIcon.js
+++ src/client/components/MaterialIcon.js
@@ -3,7 +3,7 @@ import Icon from "@material-ui/core/Icon";
 import matches from "./mediaQuery";
 
 const MaterialIcon = props => {
-  const iconSize = matches("sm") ? "md-64" : "md-48";
+  const iconSize = matches("md") ? "md-46" : "md-28";
   return <Icon classes={{ root: iconSize }}>{props.iconName}</Icon>;
 };
 
