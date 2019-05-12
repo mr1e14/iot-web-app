9f14dc66297b98f2d2747ce39cdfd72a19c991f9
mr1e14 <mr1e14@soton.ac.uk>
2019-05-12 23:23:56 +0100

    add mock lights api responses

---
+++ src/client/sample_data/lights.js
@@ -0,0 +1,66 @@
+const oneLight = [
+  {
+    name: "Bedroom",
+    connected: true,
+    on: true,
+    brightness: 100,
+    effect: null,
+    color: "#42f48f"
+  }
+];
+
+const twoLights = [
+  {
+    name: "Bedroom",
+    connected: true,
+    on: true,
+    brightness: 100,
+    effect: null,
+    color: "#42f48f"
+  },
+  {
+    name: "Hall",
+    connected: true,
+    on: false,
+    brightness: 14,
+    effect: null,
+    color: "#ff8026"
+  }
+];
+
+const fourLights = [
+  {
+    name: "Bedroom",
+    connected: true,
+    on: true,
+    brightness: 100,
+    effect: null,
+    color: "#42f48f"
+  },
+  {
+    name: "Hall",
+    connected: true,
+    on: false,
+    brightness: 100,
+    effect: null,
+    color: "#ff8026"
+  },
+  {
+    name: "Lounge",
+    connected: false,
+    on: true,
+    brightness: 55,
+    effect: null,
+    color: "#BA55D3"
+  },
+  {
+    name: "Lounge",
+    connected: true,
+    on: true,
+    brightness: 13,
+    effect: null,
+    color: "#5126ff"
+  }
+];
+
+export { oneLight, twoLights, fourLights };
