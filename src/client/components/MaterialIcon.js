9c5802b92d003a19d76968963458c0c935367ffc
mr1e14 <mr1e14@soton.ac.uk>
2019-05-16 22:10:15 +0100

    adjust MaterialIcon font size

--- src/client/components/MaterialIcon.js
+++ src/client/components/MaterialIcon.js
@@ -3,7 +3,7 @@ import Icon from "@material-ui/core/Icon";
 import matches from "./mediaQuery";
 
 const MaterialIcon = props => {
-  const iconSize = matches("md") ? "md-46" : "md-28";
+  const iconSize = matches("md") ? "md-40" : "md-28";
   return <Icon classes={{ root: iconSize }}>{props.iconName}</Icon>;
 };
 
