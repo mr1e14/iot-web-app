f9408a93a13c6e65be89b9a66351820a4eb80ed9
mr1e14 <mr1e14@soton.ac.uk>
2019-05-18 11:52:24 +0100

    Replace fade transition with slide

--- src/client/components/PageSwitch.js
+++ src/client/components/PageSwitch.js
@@ -1,24 +1,52 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, withRouter } from "react-router-dom";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import Home from "./pages/Home";
 import Lights from "./pages/Lights";
 
-const Page = props => (
-  <Route
-    render={({ location }) => (
+class PageSwitch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      previousDepth: this.getPathDepth(this.props.location)
+    };
+  }
+
+  componentWillReceiveProps() {
+    this.setState({ previousDepth: this.getPathDepth(this.props.location) });
+  }
+
+  getPathDepth(location) {
+    let path = location.pathname.split("/");
+    return path.filter(page => page !== "").length;
+  }
+
+  render() {
+    const { location } = this.props;
+    return (
       <TransitionGroup>
-        <CSSTransition key={location.key} timeout={300} classNames="fade">
-          <Switch location={location}>
-            <Route exact path="/" render={() => <Home {...props} />} />
-            <Route path="/lights" render={() => <Lights {...props} />} />
-          </Switch>
+        <CSSTransition
+          key={location.key}
+          timeout={{ enter: 700, exit: 350 }}
+          classNames="pageSlider"
+          mountOnEnter={false}
+          unmountOnExit={true}
+        >
+          <div
+            className={
+              this.getPathDepth(location) - this.state.previousDepth >= 0
+                ? "left"
+                : "right"
+            }
+          >
+            <Switch location={location}>
+              <Route exact path="/" render={() => <Home {...this.props} />} />
+              <Route path="/lights" render={() => <Lights {...this.props} />} />
+            </Switch>
+          </div>
         </CSSTransition>
       </TransitionGroup>
-    )}
-  />
-);
-
-const PageSwitch = props => <Page {...props} />;
-
-export default PageSwitch;
+    );
+  }
+}
+export default withRouter(PageSwitch);
