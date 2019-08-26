6239b8137405d2e03a23b680ee4506ea2bd7946a
mr1e14 <mr1e14@soton.ac.uk>
2019-08-26 14:56:34 +0100

    retrieve initial data in LightView

--- src/client/components/pages/LightView.js
+++ src/client/components/pages/LightView.js
@@ -3,16 +3,33 @@ import Logo from "../Logo";
 import LightController from "../LightController";
 import Grid from "@material-ui/core/Grid";
 
+const getLightData = (lightsData, id) => {
+  let lightData = null;
+  lightsData.forEach(data => {
+    if (data.id === id) {
+      lightData = data;
+    }
+  });
+  return lightData;
+};
+
 const LightView = props => {
   const {
     classes,
-    match: { params }
+    match: {
+      params: { id }
+    },
+    lightsData
   } = props;
   return (
     <div className="page">
       <Grid container className={classes.root}>
         <Logo classes={classes} />
-        <LightController classes={classes} id={params.id} />
+        <LightController
+          classes={classes}
+          id={id}
+          lightData={getLightData(lightsData, id)}
+        />
       </Grid>
     </div>
   );
