a4b42989e6a8d071962615f6ed5bf8fed3747910
mr1e14 <mr1e14@soton.ac.uk>
2019-09-06 23:34:22 +0100

    create Notification component

---
+++ src/client/components/Notification.js
@@ -0,0 +1,83 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import { amber, green, blue } from "@material-ui/core/colors";
+import IconButton from "@material-ui/core/IconButton";
+import Snackbar from "@material-ui/core/Snackbar";
+import Icon from "@material-ui/core/Icon";
+import SnackbarContent from "@material-ui/core/SnackbarContent";
+import { makeStyles } from "@material-ui/core/styles";
+
+const variantIcon = {
+  success: "check_circle",
+  warning: "warning",
+  error: "error",
+  info: "info"
+};
+
+const useStyles = makeStyles(theme => ({
+  success: {
+    backgroundColor: green[600],
+    marginRight: theme.spacing(1)
+  },
+  error: {
+    backgroundColor: theme.palette.error.dark,
+    marginRight: theme.spacing(1)
+  },
+  info: {
+    backgroundColor: blue[500],
+    marginRight: theme.spacing(1)
+  },
+  warning: {
+    backgroundColor: amber[700],
+    marginRight: theme.spacing(1)
+  },
+  icon: {
+    fontSize: 20,
+    opacity: 0.9,
+    marginRight: theme.spacing(1)
+  },
+  message: {
+    display: "flex",
+    alignItems: "center",
+    fontSize: "14px"
+  }
+}));
+
+const Notification = props => {
+  const classes = useStyles();
+  const { message, handleClose, variant, open } = props;
+
+  return (
+    <Snackbar
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "left"
+      }}
+      open={open}
+      autoHideDuration={8000}
+      onClose={handleClose}
+    >
+      <SnackbarContent
+        className={classes[variant]}
+        message={
+          <span className={classes.message}>
+            <Icon className={classes.icon}>{variantIcon[variant]}</Icon>
+            {message}
+          </span>
+        }
+        action={[
+          <IconButton
+            key="close"
+            aria-label="close"
+            color="inherit"
+            onClick={handleClose}
+          >
+            <Icon className={classes.icon}>{"close"}</Icon>
+          </IconButton>
+        ]}
+      />
+    </Snackbar>
+  );
+};
+
+export default Notification;
