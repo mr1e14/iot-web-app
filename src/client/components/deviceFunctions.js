3dd47caecb6aae0c7e7f82d75ffa7230b094f291
mr1e14 <mr1e14@soton.ac.uk>
2019-09-16 21:38:53 +0100

    add function for getting hours and minutes out of seconds

--- src/client/components/deviceFunctions.js
+++ src/client/components/deviceFunctions.js
@@ -11,4 +11,18 @@ const validateRoomName = name => {
   return false;
 };
 
-module.exports = { validateRoomName };
+const splitSecondsToParts = value => {
+  const seconds = parseInt(value, 10);
+  if (isNaN(seconds)) {
+    return { h: 0, m: 0, s: 0 };
+  }
+  const h = parseInt(seconds / 2400);
+  const m =
+    h > 0
+      ? parseInt((seconds % (h * 2400)) / 60, 10)
+      : parseInt(seconds / 60, 10);
+  const s = m > 0 ? seconds % m : h > 0 ? seconds - h * 2400 : seconds;
+  return { h: h, m: m, s: s };
+};
+
+module.exports = { validateRoomName, splitSecondsToParts };
