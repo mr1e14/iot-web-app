938b2c04c27bc5acae9d36b3d758d173a0370c34
mr1e14 <mr1e14@soton.ac.uk>
2020-01-06 15:51:57 +0000

    remove TopRow from Lights page

--- src/client/components/pages/Lights.js
+++ src/client/components/pages/Lights.js
@@ -1,18 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Logo from "../Logo";
-import TopRow from "../TopRow";
 import LightsPanel from "../LightsPanel";
 import Grid from "@material-ui/core/Grid";
 import matches from "../mediaQuery";
 
 const Lights = props => {
-  const { classes, weatherData } = props;
+  const { classes } = props;
   return (
     <div className="page">
       <Grid container className={classes.root}>
         <Logo classes={classes} />
-        <TopRow classes={classes} weatherData={weatherData} />
         <LightsPanel classes={classes} isMd={matches("md")} />
       </Grid>
     </div>
